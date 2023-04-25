121144e2fc388322659cf68038f5e45f1f635371
YevaVlasova <eva-krechetova@ukr.net>
2023-04-25 14:39:53 +0300

    added adaptability

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { Routes, Route, } from 'react-router-dom';
 import './App.scss';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import Work from './components/Work/Work';
+import Home from './pages/Home/Home';
+import Work from './pages/Work/Work';
 
 
 function App() {
--- src/pages/Work/Work.js
+++ src/pages/Work/Work.js
@@ -1,10 +1,10 @@
 import './Work.scss';
 
 import bgImage from "../../assets/p2Main.png";
-import Covibe from '../Covibe/Covibe';
-import Space from '../Space/Space';
-import Solution from '../Solution/Solution';
-import Conclusion from '../Conclusion/Conclusion';
+import Covibe from '../../components/Covibe/Covibe';
+import Space from '../../components/Space/Space';
+import Solution from '../../components/Solution/Solution';
+import Conclusion from '../../components/Conclusion/Conclusion';
 
 export default function Work() {
   return (
